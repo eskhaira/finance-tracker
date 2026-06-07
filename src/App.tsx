9be0c1eb8500e023f4e2f1cb96b38c80cba229c7
Ekam <ekampreet.khaira@gmail.com>
2026-06-07 19:34:46 -0400

    delete functionalyity added using useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-const transactions = [
-  { id: 1, description: "Diesel - Flying J", amount: 142.5 },
-  { id: 2, description: "Coffee - Tim Hortons", amount: 3.25 },
-  { id: 3, description: "Truck wash", amount: 28.0 },
-];
+import { useState } from "react";
 
 type TransactionProps = {
+  id: number;
   description: string;
   amount: number;
+  onDelete: (id: number) => void;
 };
 
-function Transaction({ description, amount }: TransactionProps) {
+function Transaction({ id, description, amount, onDelete }: TransactionProps) {
   return (
     <li>
       {description} - ${amount}
+      <button onClick={() => onDelete(id)}>Delete</button>
     </li>
   );
 }
 
 function App() {
+  const [transactions, setTransactions] = useState([
+    { id: 1, description: "Diesel - Flying J", amount: 142.5 },
+    { id: 2, description: "Coffee - Tim Hortons", amount: 3.25 },
+    { id: 3, description: "Truck wash", amount: 28.0 },
+  ]);
+
+  function handleDelete(idToRemove: number) {
+    const newTransactions = transactions.filter((t) => t.id !== idToRemove);
+    setTransactions(newTransactions);
+  }
   return (
     <div>
       <h1>Transactions</h1>
@@ -25,8 +34,10 @@ function App() {
         {transactions.map((transaction) => (
           <Transaction
             key={transaction.id}
+            id={transaction.id}
             description={transaction.description}
             amount={transaction.amount}
+            onDelete={handleDelete}
           />
         ))}
       </ul>
