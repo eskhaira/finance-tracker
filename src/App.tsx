bae0fd21b713f9129642a7d480f06b8087f0f3ce
Ekam <ekampreet.khaira@gmail.com>
2026-06-09 20:57:45 -0400

    Solo Build unfinished uploaded for review from Claude

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,10 @@ function App() {
     { id: 3, description: "Truck wash", amount: 28.0 },
   ]);
 
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const [searchText, setSearchText] = useState("");
+
   function handleDelete(idToRemove: number) {
     const newTransactions = transactions.filter((t) => t.id !== idToRemove);
     setTransactions(newTransactions);
@@ -31,9 +35,51 @@ function App() {
   function handleClearAll() {
     setTransactions([]);
   }
+
+  function handleAdd() {
+    const newTransaction = {
+      id: Date.now(),
+      description: description,
+      amount: Number(amount),
+    };
+    setTransactions([...transactions, newTransaction]);
+    setDescription("");
+    setAmount("");
+  }
+
+  function handleSearch() {
+    const newTransaction = transactions.filter(
+      (t) => t.description == searchText,
+    );
+    setTransactions(newTransaction);
+    setSearchText("");
+  }
+
   return (
     <div>
       <h1>Transactions</h1>
+      <input
+        type="text"
+        placeholder="description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <button onClick={() => handleAdd()}>Add</button>
+      <p>
+        <input
+          type="text"
+          placeholder="searchText"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button onClick={() => handleSearch()}>Search</button>
+      </p>
       <ul>
         {transactions.map((transaction) => (
           <Transaction
